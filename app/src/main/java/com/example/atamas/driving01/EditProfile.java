404e5675484525690e944ceda17b9d10e2a11cfb
unknown <haverda88@gmail.com>
2016-05-26 14:46:12 +0200

    almost done-working

--- app/src/main/java/com/example/atamas/driving01/EditProfile.java
+++ app/src/main/java/com/example/atamas/driving01/EditProfile.java
@@ -2,10 +2,15 @@ package com.example.atamas.driving01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.atamas.driving01.DAL.BEUser;
 import com.example.atamas.driving01.DAL.Users;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -19,6 +24,8 @@ public class EditProfile extends AppCompatActivity {
     TextView lastName;
     TextView email;
     TextView telNum;
+    Button btnUpdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +34,54 @@ public class EditProfile extends AppCompatActivity {
         user = (BEUser) extras.getSerializable("user");
 
 
-        firstName = (TextView)findViewById(R.id.txtEditFirstName);
-        lastName = (TextView)findViewById(R.id.txtEditLastName);
-        email = (TextView)findViewById(R.id.txtEditEmail);
-        telNum = (TextView)findViewById(R.id.txtEditTel);
+
+        firstName = (TextView) findViewById(R.id.txtEditFirstName);
+        lastName = (TextView) findViewById(R.id.txtEditLastName);
+        email = (TextView) findViewById(R.id.txtEditEmail);
+        telNum = (TextView) findViewById(R.id.txtEditTel);
 
         firstName.setText(user.getFirstName());
         lastName.setText(user.getLastName());
         email.setText(user.getEmail());
         telNum.setText(user.getTelNum());
+
+        btnUpdate = (Button)findViewById(R.id.btnUpdate);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setFirstName(firstName.getText().toString());
+                user.setLastName(lastName.getText().toString());
+                user.setEmail(email.getText().toString());
+                user.setTelNum(telNum.getText().toString());
+                EditProfile.this.onClickUpdate(user);
+            }
+        });
+
+
+
+
     }
 
+    private void onClickUpdate(BEUser user) {
+        JSONObject post_dict = new JSONObject();
 
+        try {
+
+
+            post_dict.put("firstName", user.getFirstName());
+            post_dict.put("lastName", user.getLastName());
+            post_dict.put("email", user.getEmail());
+            post_dict.put("telNum", user.getTelNum());
+            post_dict.put("_id", user.getId());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (post_dict.length() > 0) {
+            new PutUser(this).execute(String.valueOf(post_dict));
+
+        }
+
+    }
 }
